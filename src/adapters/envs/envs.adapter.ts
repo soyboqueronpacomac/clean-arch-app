10a2bd18cf47ef6e1f04420a39995843e6040196
Paco Coder <elboqueronpaco@icloud.com>
2025-09-02 13:59:36 +0200

    creacion de version api

--- src/adapters/envs/envs.adapter.ts
+++ src/adapters/envs/envs.adapter.ts
@@ -3,4 +3,5 @@ const { get } = pkg;
 
 export const envsAdapter = {
     PORT: get("PORT").default("8000").asPortNumber(),
+    API_VERSION: get('API_VERSION').default("v1").asString(),
 }
--- src/presentation/app.routes.ts
+++ src/presentation/app.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { apiRoutes } from "./api/api.routes";
+import { envsAdapter } from "../adapters";
 
 export class AppRoutes {
   static get routes(): Router{
     const routes = Router();
+    const apiVersion = envsAdapter.API_VERSION
     // Todas las rutas de la app
-    routes.use('/api', apiRoutes.routes)
+    routes.use(`/api/${apiVersion}`, apiRoutes.routes)
     return routes;
   }
 }
